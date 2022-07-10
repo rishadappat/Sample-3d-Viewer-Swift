96f25139bcbd5cbc48bcbc91ee3a6b9982517ca5
Rishad Appat <rishadappat@gmail.com>
2022-07-10 21:30:00 +0400

    Initial Commit

--- Sample3dViewer/ViewController.swift
+++ Sample3dViewer/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadWebView()
     }
 
+    private func loadWebView()
+    {
+        webView.scrollView.bounces = false
+        webView.scrollView.alwaysBounceVertical = false
+        webView.scrollView.alwaysBounceHorizontal = false
+        do{
+            guard let filePath = Bundle.main.path(forResource: "model_viewer", ofType: "html") else { return }
+            let contents = try String(contentsOfFile: filePath, encoding: .utf8)
+            let baseUrl = URL(fileURLWithPath: filePath)
+            webView.loadHTMLString(contents as String, baseURL: baseUrl)
+        } catch {
+            print("HTML File error")
+        }
+    }
 
 }
 
